f965afc18956bcbfa85d4914b6f18ede46337839
Yuhe Lu <2753619L@student.gla.ac.uk>
2025-02-12 13:21:11 +0000

    4.1 completed

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("""<h1>Rango says hey there partner!</h1>
-                        <p><a href="/rango/about/">About</a></p>""")
+    context_dict = {'boldmessage': 'Imagine all the people living life in peace'}
+
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("""<h1>Rango says here is the About page.</h1>
