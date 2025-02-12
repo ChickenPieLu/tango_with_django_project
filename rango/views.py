6475f779b0da652283959aa6262cf3b2a7fe520b
Yuhe Lu <2753619L@student.gla.ac.uk>
2025-02-12 15:14:01 +0000

    chapter 4 completed

--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context_dict = {'boldmessage': 'Imagine all the people living life in peace'}
+    context_dict = {'boldmessage': 'Apple, Banana, Coconut and Dragonfruit'}
 
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("""<h1>Rango says here is the About page.</h1>
-                        <p><a href="/rango/">Index</a></p>""")
+    context_dict = {'aboutmessage': 'This tutorial has been put together by Yuhe Lu.'}
+    return render(request, 'rango/about.html', context=context_dict)
